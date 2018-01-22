ca6dd27ecbe4089e7ff981189ea0785773d2aa75
Лоллион <@172.22.227.19>
2018-01-22 19:40:26 +0200

    Uploading Chess Game

---
+++ ChessGameFedorenko/BlackWidow-Chess-master/src/com/chess/BlackWidow.java
@@ -0,0 +1,14 @@
+package com.chess;
+
+import com.chess.gui.Table;
+
+/**
+ * @made by Yura Fedorenko
+ *
+ */
+public class BlackWidow {
+
+	public static void main(final String args[]) throws Exception {
+		Table.get().show();
+	}
+}
